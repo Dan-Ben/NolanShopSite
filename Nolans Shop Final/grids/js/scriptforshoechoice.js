03159005c98115f8af27917a334df66de1086a23
Danjooo <danielb009@hotmail.com>
2015-09-02 13:02:11 +0100

    Nolan's Shoe Store Site

---
+++ Nolans Shop Final/grids/js/scriptforshoechoice.js
@@ -0,0 +1,52 @@
+
+function brown(){
+		{if(document.getElementById('ch').checked){
+				document.getElementById('s1').src='images/brownleathershoe.jpg';
+			}
+		else {document.getElementById('s1').src='images/blackleathershoe.jpg';}
+			}
+		{if(document.getElementById('ch1').checked){
+				document.getElementById('s2').src='images/dexiebrown.jpg';
+			}
+		else {document.getElementById('s2').src='images/dexieblack.jpg';}
+			}
+		{if(document.getElementById('ch2').checked){
+				document.getElementById('s3').src='images/dorsetbrown.jpg';
+			}
+		else {document.getElementById('s3').src='images/dorsetblack.jpg';}
+			}
+		{if(document.getElementById('ch3').checked){
+				document.getElementById('s4').src='images/dutydesert.jpg';
+			}
+		else {document.getElementById('s4').src='images/dutybrown.jpg';}
+			}
+			
+			
+		{if(document.getElementById('ch4').checked){
+				document.getElementById('s5').src='images/golbybrown.jpg';
+			}
+		else {document.getElementById('s5').src='images/gobyblack.jpg';}
+			}
+		{if(document.getElementById('ch5').checked){
+				document.getElementById('s6').src='images/lacebrown.jpg';
+			}
+		else {document.getElementById('s6').src='images/lavceblack.jpg';}
+			}
+		{if(document.getElementById('ch6').checked){
+				document.getElementById('s7').src='images/limitbrown.jpg';
+			}
+		else {document.getElementById('s7').src='images/limitblack.jpg';}
+			}
+			
+			
+		{if(document.getElementById('ch7').checked){
+				document.getElementById('s8').src='images/swiftbrown.jpg';
+			}
+		else {document.getElementById('s8').src='images/swiftblack.jpg';}
+			}
+		{if(document.getElementById('ch8').checked){
+				document.getElementById('s9').src='images/ferobrown.jpg';
+			}
+		else {document.getElementById('s9').src='images/feroblack.jpg';}
+			}
+}
